0c2e768899ffe1db2cc793e13a32a05319c3e296
heisen <mclegendnew@gmail.com>
2025-08-09 17:41:10 +0200

    refactor: ContentEditor styling, form validation and ttl

--- storage/src/main/java/dev/heisen/upload/dto/PublicationRequest.java
+++ storage/src/main/java/dev/heisen/upload/dto/PublicationRequest.java
@@ -21,7 +21,7 @@ public record PublicationRequest(
 
         @NotNull
         @Min(1)
-        @Max(40320) // month in minutes
+        @Max(43830) // month in minutes
         int ttlMinutes
 ) {
 }
